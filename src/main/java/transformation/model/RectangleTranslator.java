0599b1480ac911365d8c77fcd77b504e3d74532b
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-04-23 19:14:30 +0200

    Changed to move camera not object

--- src/main/java/transformation/model/RectangleTranslator.java
+++ src/main/java/transformation/model/RectangleTranslator.java
@@ -5,37 +5,37 @@ public abstract class RectangleTranslator {
 
     public static void moveForward(Rectangle rectangle) {
         for (PointMatrix point : rectangle.vertices) {
-            MatrixOps.translate(point, 0, 0, DEFAULT_SHIFT);
+            MatrixOps.translate(point, 0, 0, -DEFAULT_SHIFT);
         }
     }
 
     public static void moveBackward(Rectangle rectangle) {
         for (PointMatrix point : rectangle.vertices) {
-            MatrixOps.translate(point, 0, 0, -DEFAULT_SHIFT);
+            MatrixOps.translate(point, 0, 0, DEFAULT_SHIFT);
         }
     }
 
     public static void moveLeft(Rectangle rectangle) {
         for (PointMatrix point : rectangle.vertices) {
-            MatrixOps.translate(point, -DEFAULT_SHIFT, 0, 0);
+            MatrixOps.translate(point, DEFAULT_SHIFT, 0, 0);
         }
     }
 
     public static void moveRight(Rectangle rectangle) {
         for (PointMatrix point : rectangle.vertices) {
-            MatrixOps.translate(point, DEFAULT_SHIFT, 0, 0);
+            MatrixOps.translate(point, -DEFAULT_SHIFT, 0, 0);
         }
     }
 
     public static void moveUp(Rectangle rectangle) {
         for (PointMatrix point : rectangle.vertices) {
-            MatrixOps.translate(point, 0, -DEFAULT_SHIFT, 0);
+            MatrixOps.translate(point, 0, DEFAULT_SHIFT, 0);
         }
     }
 
     public static void moveDown(Rectangle rectangle) {
         for (PointMatrix point : rectangle.vertices) {
-            MatrixOps.translate(point, 0, DEFAULT_SHIFT, 0);
+            MatrixOps.translate(point, 0, -DEFAULT_SHIFT, 0);
         }
     }
 }
