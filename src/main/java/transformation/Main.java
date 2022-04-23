d72faefb1b7f03ada2dbddc44be44d7e69141454
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-04-23 10:52:40 +0200

    Added basic jframe

--- src/main/java/transformation/Main.java
+++ src/main/java/transformation/Main.java
@@ -1,7 +1,22 @@
 package transformation;
 
 public class Main {
+    public final Model model;
+    public final View view;
+    public final Controller controller;
+
+    public Main() {
+        model = new Model();
+        view = new View();
+        controller = new Controller();
+    }
+
+    public void start() {
+        view.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Main main = new Main();
+        main.start();
     }
 }
--- src/main/java/transformation/View.java
+++ src/main/java/transformation/View.java
@@ -3,5 +3,9 @@ package transformation;
 import javax.swing.JFrame;
 
 public class View extends JFrame {
-
+    public View() {
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }
