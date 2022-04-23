a9f00199a9c554bba6f6b7ed9899222d6f5d9045
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-04-23 12:18:13 +0200

    Removed model and controller from main

--- src/main/java/transformation/Main.java
+++ src/main/java/transformation/Main.java
@@ -1,17 +1,12 @@
 package transformation;
 
-import transformation.model.Model;
 import transformation.view.View;
 
 public class Main {
-    public final Model model;
     public final View view;
-    public final Controller controller;
 
     public Main() {
-        model = new Model();
         view = new View();
-        controller = new Controller();
     }
 
     public void start() {
