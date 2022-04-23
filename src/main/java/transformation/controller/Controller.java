efb15ca3bae5f237e146d9f7f128f4194ab1eae5
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-04-23 13:15:42 +0200

    Added key assignments

--- src/main/java/transformation/controller/Controller.java
+++ src/main/java/transformation/controller/Controller.java
@@ -1,22 +1,68 @@
 package transformation.controller;
 
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.KeyAdapter;
 
-public class Controller implements KeyListener {
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        System.out.println("Typed " + e.getKeyChar());
-    }
+public class Controller extends KeyAdapter {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        System.out.println("Pressed " + e.getKeyChar());
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        System.out.println("Released " + e.getKeyChar());
+        switch (e.getExtendedKeyCode()) {
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_UP:
+                System.out.println("Move forward");
+                break;
+            case KeyEvent.VK_S:
+            case KeyEvent.VK_DOWN:
+                System.out.println("Move backward");
+                break;
+            case KeyEvent.VK_A:
+            case KeyEvent.VK_LEFT:
+                System.out.println("Move left");
+                break;
+            case KeyEvent.VK_D:
+            case KeyEvent.VK_RIGHT:
+                System.out.println("Move right");
+                break;
+            case KeyEvent.VK_SPACE:
+                System.out.println("Move up");
+                break;
+            case KeyEvent.VK_SHIFT:
+                System.out.println("Move down");
+                break;
+            case KeyEvent.VK_PLUS:
+            case KeyEvent.VK_ADD:
+                System.out.println("Zoom in");
+                break;
+            case KeyEvent.VK_MINUS:
+            case KeyEvent.VK_SUBTRACT:
+                System.out.println("Zoom out");
+                break;
+            case KeyEvent.VK_NUMPAD8:
+                System.out.println("Rotate backward (Look up)");
+                break;
+            case KeyEvent.VK_NUMPAD2:
+                System.out.println("Rotate forward (Look down)");
+                break;
+            case KeyEvent.VK_NUMPAD6:
+                System.out.println("Rotate right (Look right)");
+                break;
+            case KeyEvent.VK_NUMPAD4:
+                System.out.println("Rotate left (Look left)");
+                break;
+            case KeyEvent.VK_NUMPAD9:
+            case KeyEvent.VK_NUMPAD1:
+                System.out.println("Rotate clockwise");
+                break;
+            case KeyEvent.VK_NUMPAD7:
+            case KeyEvent.VK_NUMPAD3:
+                System.out.println("Rotate counterclockwise");
+                break;
+            case KeyEvent.VK_NUMPAD5:
+                System.out.println("Reset view?");
+                break;
+            default:
+                System.out.println("Do nothing " + e.getExtendedKeyCode());
+        }
     }
 }
