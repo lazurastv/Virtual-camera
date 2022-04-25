e4e6e875166e1dc1b43114be386da789d2110ed7
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-04-25 20:07:47 +0200

    Centered scene

--- src/main/java/transformation/model/Scene.java
+++ src/main/java/transformation/model/Scene.java
@@ -1,13 +1,13 @@
 package transformation.model;
 
 public abstract class Scene {
-        public static final Rectangle RIGHT_FRONT_HOUSE = new Rectangle(200, 600, 400, 600, -400, 800);
+        public static final Rectangle RIGHT_FRONT_HOUSE = new Rectangle(500, 600, 3400, 900, -400, 3800);
 
-        public static final Rectangle RIGHT_BACK_HOUSE = new Rectangle(400, 600, 2000, 900, -1400, 2500);
+        public static final Rectangle RIGHT_BACK_HOUSE = new Rectangle(700, 600, 5000, 1200, -1400, 5500);
 
-        public static final Rectangle LEFT_FRONT_HOUSE = new Rectangle(-1200, 600, 300, -400, -700, 1000);
+        public static final Rectangle LEFT_FRONT_HOUSE = new Rectangle(-900, 600, 3300, -100, -700, 4000);
 
-        public static final Rectangle LEFT_BACK_HOUSE = new Rectangle(-2500, 600, 1200, -1000, 100, 1500);
+        public static final Rectangle LEFT_BACK_HOUSE = new Rectangle(-2200, 600, 4200, -700, 100, 4500);
 
         public static final Rectangle[] DEFAULT_VIEW = new Rectangle[] {
                         LEFT_FRONT_HOUSE,
