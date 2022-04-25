bd0c359a384238869daaca0698d5a3723e2eb50a
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-04-25 19:19:38 +0200

    Added non num lock controls

--- src/main/java/transformation/controller/Controller.java
+++ src/main/java/transformation/controller/Controller.java
@@ -22,19 +22,15 @@ public class Controller extends KeyAdapter {
     public void keyPressed(KeyEvent e) {
         switch (e.getExtendedKeyCode()) {
             case KeyEvent.VK_W:
-            case KeyEvent.VK_UP:
                 camera.transformCamera(CameraOps.MoveForward);
                 break;
             case KeyEvent.VK_S:
-            case KeyEvent.VK_DOWN:
                 camera.transformCamera(CameraOps.MoveBackward);
                 break;
             case KeyEvent.VK_A:
-            case KeyEvent.VK_LEFT:
                 camera.transformCamera(CameraOps.MoveLeft);
                 break;
             case KeyEvent.VK_D:
-            case KeyEvent.VK_RIGHT:
                 camera.transformCamera(CameraOps.MoveRight);
                 break;
             case KeyEvent.VK_SPACE:
@@ -45,31 +41,39 @@ public class Controller extends KeyAdapter {
                 break;
             case KeyEvent.VK_PLUS:
             case KeyEvent.VK_ADD:
+            case KeyEvent.VK_Z:
                 camera.transformProjection(ProjectionOps.ZoomIn);
                 break;
             case KeyEvent.VK_MINUS:
             case KeyEvent.VK_SUBTRACT:
+            case KeyEvent.VK_X:
                 camera.transformProjection(ProjectionOps.ZoomOut);
                 break;
             case KeyEvent.VK_NUMPAD8:
+            case KeyEvent.VK_UP:
                 camera.transformCamera(CameraOps.LookUp);
                 break;
             case KeyEvent.VK_NUMPAD5:
             case KeyEvent.VK_NUMPAD2:
+            case KeyEvent.VK_DOWN:
                 camera.transformCamera(CameraOps.LookDown);
                 break;
             case KeyEvent.VK_NUMPAD6:
+            case KeyEvent.VK_RIGHT:
                 camera.transformCamera(CameraOps.LookRight);
                 break;
             case KeyEvent.VK_NUMPAD4:
+            case KeyEvent.VK_LEFT:
                 camera.transformCamera(CameraOps.LookLeft);
                 break;
             case KeyEvent.VK_NUMPAD9:
             case KeyEvent.VK_NUMPAD3:
+            case KeyEvent.VK_E:
                 camera.transformCamera(CameraOps.RotateClockwise);
                 break;
             case KeyEvent.VK_NUMPAD7:
             case KeyEvent.VK_NUMPAD1:
+            case KeyEvent.VK_Q:
                 camera.transformCamera(CameraOps.RotateCounterclockwise);
                 break;
             case KeyEvent.VK_BACK_SPACE:
